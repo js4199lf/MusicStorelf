120b02b1f3e49424e3a2d4fbeaae606643b83e35
Administrator <Administrator@PC-20160110BMVM>
2016-07-31 11:11:02 +0800

    新建

---
+++ M1/M1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(M1.Startup))]
+namespace M1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
